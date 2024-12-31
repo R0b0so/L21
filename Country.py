4e15fabfeafa4f5fa1828e85cc20c3423b74949a
R0b0so <ethansagar222@gmail.com>
2024-12-30 19:00:03 -0800

    hello

---
+++ Country.py
@@ -0,0 +1,5 @@
+Country = {'India': '0091', 'Canada': '00987', 'Nepal': '00977', 'China': '00931'}
+print("India's country code")
+print(Country.get("India", "Not Found"))
+print("Australia's country code")
+print(Country.get("Australia", "Not Found"))
